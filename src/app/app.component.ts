9822443dc0c1249a27cfe1245b00be83de85d904
Johnson Fu <johnson.fu@gmail.com>
2018-11-05 09:35:32 +0800

    test numeric operation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngdemo';
+
+    num1: number;
+
+    num2: number;
+
+    result: number;
+
+    add() {
+
+        this.result = this.num1 + this.num2;
+
+    }
+
+    substract() {
+
+        this.result = this.num1 - this.num2;
+
+    }
+
+    multiply() {
+
+        this.result = this.num1 * this.num2;
+
+    }
+
+    divide() {
+
+        this.result = this.num1 % this.num2;
+
+    }
 }
