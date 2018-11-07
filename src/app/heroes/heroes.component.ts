343fbeec21bc42c5a27a79c19cb4dc4d71c5f13f
Johnson Fu <johnson.fu@chubb.com>
2018-11-07 14:40:57 +0800

    add services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -17,26 +19,39 @@ export class HeroesComponent implements OnInit {
 
   selectedHero: Hero;
 
-     heroes: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+  // heroes: Hero[] = [
+  //   { id: 11, name: 'Mr. Nice' },
+  //   { id: 12, name: 'Narco' },
+  //   { id: 13, name: 'Bombasto' },
+  //   { id: 14, name: 'Celeritas' },
+  //   { id: 15, name: 'Magneta' },
+  //   { id: 16, name: 'RubberMan' },
+  //   { id: 17, name: 'Dynama' },
+  //   { id: 18, name: 'Dr IQ' },
+  //   { id: 19, name: 'Magma' },
+  //   { id: 20, name: 'Tornado' }
+  // ];
 
-  constructor() { }
+  // heroes: Hero[] = HEROES;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
-   onSelect(hero: Hero): void {
+  onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  // getHeroes(): void {
+  //  this.heroes = this.heroService.getHeroes();
+  // }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
