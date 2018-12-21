f7328d942fdfa73ce7bd39fac27b2d8d5640849c
Johnson Fu <johnson.fu@chubb.com>
2018-12-21 18:11:42 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,12 @@ import * as _ from 'lodash';
 export class AppComponent {
   title = 'ngdemo';
 
-  // ngOnInit(): void {
-  //   const scores: number[] = [100, 99, 98];
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit(): void {
+    const scores: number[] = [100, 99, 98, 97];
 
-  //   _.remove(scores, 2);
-  //   scores.forEach((score) => console.log(score));
-  // }
+    _.remove(scores, 2);
+    scores.forEach((score) => console.log(score));
+  }
 
 }
