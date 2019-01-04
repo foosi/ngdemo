97e3ebee0ed9a7f3b0398587c8d7e626978a5be3
Johnson Fu <johnson.fu@chubb.com>
2019-01-04 16:55:06 +0800

    eval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 
 import * as _ from 'lodash';
+import { HttpXsrfCookieExtractor } from '@angular/common/http/src/xsrf';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
+
 export class AppComponent {
   title = 'ngdemo';
+
+  add(a, b) {
+    return a + b;
+  }
+
+  evalStringExpression(expression: string, argNames: string[]) {
+    try {
+      return Function(...argNames, `return ${expression};`) as any;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   // tslint:disable-next-line:use-life-cycle-interface
   ngOnInit(): void {
@@ -16,6 +33,16 @@ export class AppComponent {
 
     _.remove(scores, 2);
     scores.forEach((score) => console.log(score));
+
+    console.log('call add function ' + this.add(0.99, 99999));
+
+    console.log('call eval ' + this.evalStringExpression('1==1', ['model', 'formState']));
+
+    const xx = this.evalStringExpression('1==1', ['model', 'formState']);
+
+    console.log('call real eval ' + xx(10, 20));
+
   }
+
 
 }
